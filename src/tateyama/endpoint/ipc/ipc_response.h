c4f9db53869df8c791e09b60a4c9fcf8f20054b4
Takashi Horikawa <horikawa@nautilus-technologies.com>
2021-09-22 16:31:32 +0900

    changed to flush response even on tateyama::api::endpoint::response_code::application_error

--- src/tateyama/endpoint/ipc/ipc_response.h
+++ src/tateyama/endpoint/ipc/ipc_response.h
@@ -129,7 +129,7 @@ private:
     response_box::response& response_box_;
     tateyama::common::wire::garbage_collector* garbage_collector_;
 
-    tateyama::api::endpoint::response_code response_code_{};
+    tateyama::api::endpoint::response_code response_code_{tateyama::api::endpoint::response_code::started};
     std::string message_{};
 
     std::unique_ptr<ipc_data_channel> data_channel_{};
