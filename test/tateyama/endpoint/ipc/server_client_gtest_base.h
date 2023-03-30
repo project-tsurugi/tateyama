56ec14079f9c4d6a9f6af1f5a11287414bf72de9
Katsuhiko Ohkubo <ohkubo@nautilus-technologies.com>
2023-03-30 10:23:11 +0900

    use #pragma once

--- test/tateyama/endpoint/ipc/server_client_gtest_base.h
+++ test/tateyama/endpoint/ipc/server_client_gtest_base.h
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma
+#pragma once
 #include <gtest/gtest.h>
 #include "server_client_base.h"
 
--- test/tateyama/endpoint/ipc/watch_dog.h
+++ test/tateyama/endpoint/ipc/watch_dog.h
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma
+#pragma once
 #include <chrono>
 #include <condition_variable>
 #include <iostream>
