5b49263e7be7f21addd248fc9ee3e49bab40aa51
Katsuhiko Ohkubo <ohkubo@nautilus-technologies.com>
2023-04-03 09:49:07 +0900

    revise show_result_entry()

--- bench/data_channel_write/data_channel_write.cpp
+++ bench/data_channel_write/data_channel_write.cpp
@@ -75,6 +75,10 @@ public:
     }
 
     void show_result_entry(double sec, double msg_num_per_sec, double gb_per_sec) {
+        std::cout << nworker_;
+        std::cout << "," << (nthread_ > 0 ? "mt" : "mp"); // NOLINT
+        std::cout << "," << write_len_;
+        std::cout << "," << write_nloop_;
         std::cout << "," << std::fixed << std::setprecision(3) << sec;
         std::cout << "," << std::fixed << std::setprecision(1) << msg_num_per_sec;
         std::cout << "," << std::fixed << std::setprecision(2) << gb_per_sec;
@@ -83,11 +87,6 @@ public:
 
     void server() override {
         server_client_bench_base::server();
-        //
-        std::cout << nworker_;
-        std::cout << "," << (nthread_ > 0 ? "mt" : "mp"); // NOLINT
-        std::cout << "," << write_len_;
-        std::cout << "," << write_nloop_;
         //
         std::size_t msg_num = write_nloop_ * nworker_;
         std::size_t len_sum = msg_num * write_len_;
