5cc35bd9cddcde4f945c9c5831bf7206b6f8c502
Ryoji Kurosawa <kurosawa@nautilus-technologies.com>
2023-11-30 22:35:03 +0900

    remove sql tasked_write cfg. parameter

--- test/tateyama/utils/test_utils.h
+++ test/tateyama/utils/test_utils.h
@@ -57,7 +57,6 @@ static constexpr std::string_view default_configuration_for_tests {  // NOLINT
         "default_partitions=5\n"
         "stealing_wait=1\n"
         "task_polling_wait=0\n"
-        "tasked_write=true\n"
         "lightweight_job_level=0\n"
         "enable_hybrid_scheduler=true\n"
 
