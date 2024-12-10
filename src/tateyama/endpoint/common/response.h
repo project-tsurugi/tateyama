afdc000a9fa10ed0d67bb5c838c8da6ea259df84
t-horikawa <horikawa@nautilus-technologies.com>
2024-12-10 15:23:39 +0900

    further revise endpoint diagnostics

--- src/tateyama/endpoint/common/response.h
+++ src/tateyama/endpoint/common/response.h
@@ -17,6 +17,7 @@
 
 #include <atomic>
 #include <memory>
+#include <string_view>
 
 #include <glog/logging.h>
 #include <tateyama/logging.h>
@@ -54,7 +55,53 @@ public:
         completed_.store(true);
     }
 
+    [[nodiscard]] constexpr inline std::string_view state_label() noexcept {
+        using namespace std::string_view_literals;
+
+        switch (state_) {
+        case state::no_data_channel: return "data channel is not used"sv;
+        case state::to_be_used: return "data channel is to be used"sv;
+        case state::acquired: return "data channel is acquired"sv;
+        case state::released: return "data channel is released"sv;
+        case state::acquire_failed: return "acqure_channel failed"sv;
+        case state::release_failed: return "release_channel failed"sv;
+        }
+        std::abort();
+    }
+
 protected:
+    enum class state : std::uint32_t {
+        /**
+         * @briqef data channel is not used.
+         */
+        no_data_channel = 0,
+
+        /**
+         * @brief response::body_head() has been executed, response::acqure_channel() has not been executed.
+         */
+        to_be_used,
+
+        /**
+         * @brief response::acqure_channel() has been successfully executed.
+         */
+        acquired = 2,
+
+        /**
+         * @brief response::release_channel() has been successfully executed.
+         */
+        released = 3,
+
+        /**
+         * @brief response::acqure_channel() failed.
+         */
+        acquire_failed = 4,
+
+        /**
+         * @brief  response::release_channel() failed.
+         */
+        release_failed = 5
+    };
+
     std::size_t index_; // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes,misc-non-private-member-variables-in-classes)
 
     std::size_t session_id_{};  // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes,misc-non-private-member-variables-in-classes)
@@ -63,8 +110,14 @@ protected:
 
     std::atomic_bool completed_{};  // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes,misc-non-private-member-variables-in-classes)
 
+    void set_state(state s) {
+        state_ = s;
+    }
+
 private:
     bool cancel_{};
+
+    state state_{state::no_data_channel};
 };
 
 }  // tateyama::endpoint::common
