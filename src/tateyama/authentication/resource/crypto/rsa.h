0e8f1c3d2e7d35dfbd0aaf5e7a8a1f9fb2295634
t-horikawa <horikawa@nautilus-technologies.com>
2025-06-18 16:02:05 +0900

    fix  clang-tidy warnings (2nd)

--- src/tateyama/authentication/resource/crypto/rsa.h
+++ src/tateyama/authentication/resource/crypto/rsa.h
@@ -68,11 +68,11 @@ public:
         if (EVP_PKEY_decrypt_init_ex(ctx.get(), params.data()) <= 0) {
             throw std::runtime_error("fail to EVP_PKEY_decrypt_init_ex");
         }
-        if (EVP_PKEY_decrypt(ctx.get(), nullptr, &buf_len, reinterpret_cast<const unsigned char*>(in.data()), in.length()) <= 0) {
+        if (EVP_PKEY_decrypt(ctx.get(), nullptr, &buf_len, reinterpret_cast<const unsigned char*>(in.data()), in.length()) <= 0) {  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) due to openssl API
             throw std::runtime_error("fail to EVP_PKEY_decrypt first");
         }
         out.resize(buf_len);
-        if (EVP_PKEY_decrypt(ctx.get(), reinterpret_cast<unsigned char*>(out.data()), &buf_len, reinterpret_cast<const unsigned char*>(in.data()), in.length()) <= 0) {
+        if (EVP_PKEY_decrypt(ctx.get(), reinterpret_cast<unsigned char*>(out.data()), &buf_len, reinterpret_cast<const unsigned char*>(in.data()), in.length()) <= 0) {  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) due to openssl API
             throw std::runtime_error("fail to EVP_PKEY_decrypt final");
         }
         out.resize(buf_len);
@@ -128,7 +128,7 @@ private:
 
     EVP_PKEY *get_key(const std::string& pem_key_string, int selection) {
         EVP_PKEY *pkey = nullptr;
-        const auto *data = reinterpret_cast<const unsigned char*>(pem_key_string.data());
+        const auto *data = reinterpret_cast<const unsigned char*>(pem_key_string.data());  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) due to openssl API
         size_t data_len = pem_key_string.length();
 
         auto dctx = make_handle(OSSL_DECODER_CTX_new_for_pkey(&pkey, "PEM", nullptr, "RSA",
